bbaa3bb9f89cb734241b346e78d24cd548b711e8
Shafiya <skshafiya006@gmail.com>
2026-06-02 16:44:03 +0530

    Create dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Dashboard from "./components/Dashboard";
+
 function App() {
   return (
     <div>
       <h1>Personal Finance Analytics Dashboard</h1>
+
+      <Dashboard />
     </div>
   );
 }
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,31 @@
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+
+      <div>
+        <div>
+          <h3>Total Budget</h3>
+          <p>₹10000</p>
+        </div>
+
+        <div>
+          <h3>Total Spent</h3>
+          <p>₹0</p>
+        </div>
+
+        <div>
+          <h3>Remaining Budget</h3>
+          <p>₹10000</p>
+        </div>
+
+        <div>
+          <h3>Financial Health</h3>
+          <p>100/100</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
